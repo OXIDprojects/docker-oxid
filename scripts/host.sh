de9e8790c28ebe4bf08e962ae3abe82bf556cbd4
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2020-01-16 23:49:38 +0100

    moved to scripts folder
    and added description

--- scripts/host.sh
+++ scripts/host.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 # GPL 3 or newer
-
+# this is a polyfill for a docker host alias
+# host.docker.internal on linux systems
 if getent hosts host.docker.internal
 then
-   echo -e "\n it already exists"
+   echo -e "\n host.docker.internal already exists"
 else
    echo -e "`/sbin/ip route|awk '/default/ { print $3 }'`\thost.docker.internal" >> /etc/hosts
    echo "added host.docker.internal to hosts"
