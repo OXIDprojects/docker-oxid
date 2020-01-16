6fa931fb4e0ef40beebff198f1a71b054415ad81
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2020-01-16 23:04:59 +0100

    script to support host entry on linux
    host.docker.internal

---
+++ host.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# GPL 3 or newer
+
+if getent hosts host.docker.internal
+then
+   echo -e "\n it already exists"
+else
+   echo -e "`/sbin/ip route|awk '/default/ { print $3 }'`\thost.docker.internal" >> /etc/hosts
+   echo "added host.docker.internal to hosts"
+fi
